ae869439935fb6a79ad73801b52b6e21423c098d
man4ish <mandecent.gupta@gmail.com>
2019-12-20 14:51:22 -0500

    add annotation script modified

--- add_go_smart_desc.pl
+++ add_go_smart_desc.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
 
@@ -9,9 +10,13 @@ while (my $input = <$fh>)
    chomp($input);
    my @rec=();
    @rec = split("\t",$input);
-   $deschash{$rec[0]}=$rec[1];
+   if(defined $rec[0])
+   {
+      $deschash{$rec[0]}=$rec[1];
+   }
 }
 close($fh);
+
 
 open (my $fgh,$ARGV[1]) or die "could not open genelist file\n";
 while (my $ginput = <$fgh>)
@@ -21,13 +26,19 @@ while (my $ginput = <$fgh>)
    @grec = split("\t",$ginput);
    if (defined $grec[2]) 
    {
-      print("$grec[0]\t$grec[1]\t$grec[2]\t".$deschash{$grec[2]}."\n");
-      #print("$grec[0]\t$grec[1]\t\t\n");
+      my $id;
+      if(defined $grec[0]) {$id=$grec[0];} else {$id=".";}
+      if(defined $grec[1])
+      {
+         print("$grec[0]\t$grec[1]\t$grec[2]\t".$deschash{$grec[2]}."\n");
+      } 
+      else {
+         print("$id\t\t$grec[2]\t".$deschash{$grec[2]}."\n");
+      }
    } 
    else 
    {
       print("$grec[0]\t$grec[1]\t\t\n");
-      #print("$grec[0]\t$grec[1]\t$grec[2]\t".$deschash{$grec[2]}."\n");
    }
 }
 close($fgh);
